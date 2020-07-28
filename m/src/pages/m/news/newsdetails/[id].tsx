60b09e19896c1bd57fcf5b48a1d494cc4287f1e4
fay <869770665@qq.com>
2020-07-28 15:06:01 +0800

    upd

---
+++ m/src/pages/m/news/newsdetails/[id].tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '@/app/layout';
+import App from '@/app/home/news/detail';
+
+export default () => {
+  return (
+    <Layout>
+      <App/>
+    </Layout>
+  )
+}
