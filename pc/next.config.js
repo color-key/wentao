dd687cd98be59c10c0c8362eae52446ea47d0328
fay <869770665@qq.com>
2020-07-28 12:37:18 +0800

    upd

--- pc/next.config.js
+++ pc/next.config.js
@@ -2,7 +2,7 @@
  * Create by fay on 4/27/20 11:59 下午
  */
 const withNextConfig = require('@fay-next/config/next/config');
-const assetPrefix = process.env.NODE_ENV === 'production' ? '/pc' : '';
+const assetPrefix = process.env.NODE_ENV === 'production' ? '' : '';
 
 module.exports = withNextConfig({
   assetPrefix,
